5e2d4b32a0dd87e60241c980c2f7fae86a7f2518
wshrzzz <hundunbingjing@qq.com>
2014-05-21 17:36:36 +0800

    Add TimerBasic class.

---
+++ Assets/Test/Scripts/TimerBasicTest.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+using Wshrzzz.UnityUtil;
+
+public class TimerBasicTest : MonoBehaviour {
+
+    TimerBasic timer1;
+    TimerBasic timer2;
+    TimerBasic timer3;
+
+	// Use this for initialization
+	void Start () {
+        timer1 = TimerBasic.InitTimer(gameObject);
+        timer2 = TimerBasic.InitTimer(gameObject);
+        timer3 = TimerBasic.InitTimer(gameObject);
+
+        timer1.StartCount(stopInTheEnd: false);
+        timer2.StartCount(time: 3f);
+        timer3.StartCount(stopInTheEnd: false);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Debug.Log("Timer1 : " + timer1.CountTime);
+        Debug.Log("Timer2 : " + timer2.CountTime);
+        Debug.Log("Timer3 : " + timer3.CountTime);
+
+        if (!timer2.IsCounting)
+        {
+            timer2.StartCount(time: 3f);
+            if (timer3.IsCounting)
+            {
+                timer3.StopCount();
+            }
+            else
+            {
+                timer3.StartCount(reset: false);
+            }
+        }
+	}
+}
