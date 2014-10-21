72b334fc7c7016ded83ad7932c597cc8b87da26b
wshrzzz <hundunbingjing@qq.com>
2014-10-21 14:41:47 +0800

    Add ConsoleBasic class.

---
+++ Assets/wshrzzz/Scripts/ConsoleBasic.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace Wshrzzz.UnityUtil
+{
+    public class ConsoleBasic : MonoBehaviour
+    {
+        private static ConsoleBasic s_Instance = null;
+        public static ConsoleBasic Instance
+        {
+            get
+            {
+                if (s_Instance != null)
+                {
+                    return s_Instance;
+                }
+                else
+                {
+                    s_Instance = FindObjectOfType<ConsoleBasic>();
+                    if (s_Instance != null)
+                    {
+                        return s_Instance;
+                    }
+                    else
+                    {
+                        GameObject go = new GameObject();
+                        s_Instance = go.AddComponent<ConsoleBasic>();
+                        return s_Instance;
+                    }
+                }
+            }
+        }
+    }
+}
+
