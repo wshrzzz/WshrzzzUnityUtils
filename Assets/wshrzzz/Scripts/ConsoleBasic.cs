e0a4a9115c78da30be1d8b7d5287a2c52917839c
wshrzzz <hundunbingjing@qq.com>
2014-10-23 10:42:33 +0800

    Add ConsoleBasic class (Uncompleted)

--- Assets/wshrzzz/Scripts/ConsoleBasic.cs
+++ Assets/wshrzzz/Scripts/ConsoleBasic.cs
@@ -5,6 +5,22 @@ namespace Wshrzzz.UnityUtil
 {
     public class ConsoleBasic : MonoBehaviour
     {
+        void OnGUI()
+        {
+            GUILayout.BeginArea(new Rect(0, Screen.height - 25, Screen.width, 25));
+            s_ConsoleStr = GUILayout.TextField(s_ConsoleStr);
+            GUILayout.EndArea();
+        }
+
+        void Update()
+        {
+            if (s_ConsoleStr != "" && s_ConsoleStr.Substring(s_ConsoleStr.Length - 1, 1) == "\n")
+            {
+                Debug.Log("asdf");
+            }
+        }
+
+        private static string s_ConsoleStr = "";
         private static ConsoleBasic s_Instance = null;
         public static ConsoleBasic Instance
         {
@@ -16,17 +32,9 @@ namespace Wshrzzz.UnityUtil
                 }
                 else
                 {
-                    s_Instance = FindObjectOfType<ConsoleBasic>();
-                    if (s_Instance != null)
-                    {
-                        return s_Instance;
-                    }
-                    else
-                    {
-                        GameObject go = new GameObject();
-                        s_Instance = go.AddComponent<ConsoleBasic>();
-                        return s_Instance;
-                    }
+                    GameObject go = new GameObject();
+                    s_Instance = go.AddComponent<ConsoleBasic>();
+                    return s_Instance;
                 }
             }
         }
